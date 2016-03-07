b5d8132cd4c4981a3bc27ef38d00b4824815bd55
jtvanlew <jonvanlew@gmail.com>
2016-03-06 18:52:37 -0800

    adds mockup layout 1 to mirus-energy

---
+++ mirus-energy/app/assets/javascripts/application.js
@@ -0,0 +1,41 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require jquery.min
+//= require jquery-migrate.min
+//= require bootstrap.min
+//= require smoothScroll
+//= require jquery.easing.min
+//= require pace.min
+//= require jquery.parallax
+//= require waypoints.min
+//= require jquery.counterup.min
+//= require owl.carousel
+//= require jquery.form.min
+//= require jquery.validate.min
+//= require jquery.themepunch.tools.min
+//= require jquery.themepunch.revolution.min
+//= require jquery.cubeportfolio.min
+//= require gmap.js
+//= require one.app
+//= require forms/login
+//= require forms/contact
+//= require pages/page_contacts
+//= require plugins/pace-loader
+//= require plugins/owl-carousel
+//= require plugins/style-switcher
+//= require plugins/revolution-slider
+//= require plugins/cube-portfolio/cube-portfolio-lightbox
---
+++ mirus-energy/app/assets/javascripts/pages/page_contacts.js
@@ -0,0 +1,37 @@
+var ContactPage = function () {
+
+    return {
+
+    	//Basic Map
+        initMap: function () {
+			var map;
+			$(document).ready(function(){
+			  map = new GMaps({
+				div: '#map',
+				scrollwheel: false,
+				lat: 34.0688504,
+				lng: -118.4460652
+			  });
+
+			  var marker = map.addMarker({
+				lat: 34.0688504,
+				lng: -118.4460652,
+	            title: 'Mirus Energy'
+		       });
+			});
+        },
+
+        //Panorama Map
+        initPanorama: function () {
+		    var panorama;
+		    $(document).ready(function(){
+		      panorama = GMaps.createPanorama({
+		        el: '#panorama',
+		        lat : 40.748866,
+		        lng : -73.988366
+		      });
+		    });
+		}
+
+    };
+}();
